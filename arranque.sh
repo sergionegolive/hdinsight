d2af9cc9f4afcacca5109107c37924739b5e0305
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 13:21:23 +0200

    Update arranque.sh

--- arranque.sh
+++ arranque.sh
@@ -1,4 +1,5 @@
 echo "Empiezo el scritp"
 sudo apt-get --assume-yes install openvpn
+echo "Instalado OPENVPN"
 sudo openvpn --config /home/sshuser/Congelados\ de\ Navarra\ sergio.ovpn
 echo "Acabo el scritp"
