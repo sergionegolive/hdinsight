731eb5a40c8fd818956cb3e18bb8c3721f23e50b
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 13:15:30 +0200

    Update arranque.sh

--- arranque.sh
+++ arranque.sh
@@ -1,5 +1,9 @@
-echo "copio fichero a sshuser"
-hdfs dfs -copyToLocal /recursos/scriptarranque.sh /home/sshuser/
-echo "se arranca el script interno"
-bash scriptarranque.sh
+echo "copio fichero a auth"
+sudo hdfs dfs -copyToLocal /recursos/auth.txt /home/sshuser/
+echo "copio fichero a Congelados"
+sudo hdfs dfs -copyToLocal /recursos/Congelados.ovpn /home/sshuser/
+echo "copio fichero a ojdbc6"
+sudo hdfs dfs -copyToLocal /recursos/ojdbc6.jar /usr/hdp/current/sqoop-client/lib/
+echo "copio fichero a tnsnames"
+sudo hdfs dfs -copyToLocal /recursos/tnsnames.ora /home/sshuser/
 echo "llego al final, falta exit"
