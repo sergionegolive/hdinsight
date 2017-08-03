1a449cfd1673da2941259235d7340511a1fe7c23
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 12:50:41 +0200

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,4 +1,4 @@
 hdfs dfs -copyToLocal /recursos/auth.txt /home/sshuser/
-hdfs dfs -copyToLocal /recursos/Congelados\ de\ Navarra\ sergio.ovpn /home/sshuser/
+hdfs dfs -copyToLocal /recursos/Congelados.ovpn /home/sshuser/
 hdfs dfs -copyToLocal /recursos/ojdbc6.jar /usr/hdp/current/sqoop-client/lib/
 hdfs dfs -copyToLocal /recursos/tnsnames.ora /home/sshuser/
