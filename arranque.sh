7dea0cc0e7e7c74bc0134952beb3554a1d046702
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 13:20:55 +0200

    Update and rename openvpn.sh to arranque.sh

---
+++ arranque.sh
@@ -0,0 +1,4 @@
+echo "Empiezo el scritp"
+sudo apt-get --assume-yes install openvpn
+sudo openvpn --config /home/sshuser/Congelados\ de\ Navarra\ sergio.ovpn
+echo "Acabo el scritp"
--- openvpn.sh
+++
@@ -1,4 +0,0 @@
-hdfs dfs -copyToLocal /recursos/auth.txt /home/sshuser/
-hdfs dfs -copyToLocal /recursos/Congelados.ovpn /home/sshuser/
-hdfs dfs -copyToLocal /recursos/ojdbc6.jar /usr/hdp/current/sqoop-client/lib/
-hdfs dfs -copyToLocal /recursos/tnsnames.ora /home/sshuser/
