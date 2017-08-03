33c290fa4172a7e4fec3c38057832be204bc7a56
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 11:51:19 +0200

    Update arranque.sh

--- arranque.sh
+++ arranque.sh
@@ -1,3 +1,5 @@
+echo "copio fichero a sshuser"
 hdfs dfs -copyToLocal /recursos/scriptarranque.sh /home/sshuser/
-
+echo "se arranca el script interno"
 bash scriptarranque.sh
+echo "llego al final, falta exit"
