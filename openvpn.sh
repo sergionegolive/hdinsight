056beee5bcad37116db03d9580a998b07001abec
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 11:15:44 +0200

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -2,6 +2,4 @@ hdfs dfs -copyToLocal /recursos/auth.txt /home/sshuser/
 hdfs dfs -copyToLocal /recursos/Congelados\ de\ Navarra\ sergio.ovpn /home/sshuser/
 hdfs dfs -copyToLocal /recursos/ojdbc6.jar /usr/hdp/current/sqoop-client/lib/
 hdfs dfs -copyToLocal /recursos/tnsnames.ora /home/sshuser/
-apt-get --assume-yes install openvpn
-openvpn --config /home/sshuser/Congelados\ de\ Navarra\ sergio.ovpn
 
