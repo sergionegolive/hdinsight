cbfdf740e7ad0f61deb9daad058acaf842d5fa0e
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-03 14:25:43 +0200

    Update final.sh

--- final.sh
+++ final.sh
@@ -6,4 +6,8 @@ echo "copio fichero a ojdbc6"
 sudo hdfs dfs -copyToLocal /recursos/ojdbc6.jar /usr/hdp/current/sqoop-client/lib/
 echo "copio fichero a tnsnames"
 sudo hdfs dfs -copyToLocal /recursos/tnsnames.ora /home/sshuser/
-echo "llego al final, falta exit"
+echo "Empiezo el scritp para instalar openvpn"
+sudo apt-get --assume-yes install openvpn
+echo "Instalado OPENVPN"
+nohup sudo openvpn --config /home/sshuser/Congelados.ovpn &
+echo "Acabo el scritp"
