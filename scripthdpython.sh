8f3f209b0f08c8952abb1ef88736f3162929950a
sergionegolive <30461750+sergionegolive@users.noreply.github.com>
2017-08-10 14:23:21 +0200

    Update scripthdpython.sh

--- scripthdpython.sh
+++ scripthdpython.sh
@@ -1,4 +1,3 @@
 /usr/bin/anaconda/bin/conda install -c conda-forge tensorflow
-/usr/bin/anaconda/bin/conda install -c conda-forge keras tensorflow theano gensim -n py35
 sudo /usr/bin/anaconda/bin/conda update conda
 sudo /usr/bin/anaconda/bin/conda install scikit-learn=0.18.2
